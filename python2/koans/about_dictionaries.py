ecfa575fe91cb943954b5d12ccfa7d41a42d0b58
uziela <karolis.uziela@scilifelab.se>
2013-02-13 15:20:48 +0100

    empty dictionary test

--- python2/koans/about_dictionaries.py
+++ python2/koans/about_dictionaries.py
@@ -13,7 +13,7 @@ class AboutDictionaries(Koan):
         empty_dict = dict()
         self.assertEqual(dict, type(empty_dict))
         self.assertEqual(dict(), empty_dict)
-        self.assertEqual(__, len(empty_dict))
+        self.assertEqual(0, len(empty_dict))
         
     def test_dictionary_literals(self):
         empty_dict = {}
